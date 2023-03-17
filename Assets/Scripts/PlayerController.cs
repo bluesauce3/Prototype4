7b67d0101e0689ea88c4111317591ac44d4725b1
Theo <106856604+bluesauce3@users.noreply.github.com>
2023-03-17 17:34:07 +1300

    added enemy movement and powerup collection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private RigidBody playerRb;
+    private Rigidbody playerRb;
     private GameObject focalPoint;
     public float speed = 5.0f;
+    public bool hasPowerup;
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<RigidBody>();
+        playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
 
     }
@@ -18,7 +19,32 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float forwardInput = input.GetAxis("Vertical");
+        float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+    
+    private float powerupStrength = 15.0f;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup) {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Collided with " + collision.gameObject.name
+             + " with powerup set to " + hasPowerup);
+             enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+
+        
     }
 }
